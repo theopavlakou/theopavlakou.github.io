427d6940d9b8c903a8e2ac4c44b502caca53e7a4
Theo Pavlakou <theo.pavlakou@gmail.com>
2016-04-08 08:14:50 +0100

    Homepage: Started code for SVD tutorial

--- code/svd/demo.py
+++ code/svd/demo.py
@@ -8,7 +8,7 @@ from os.path import join
 
 cwd = os.getcwd()
 fig, ax = plt.subplots(2, 2)
-fig.set_size_inches(18.5, 10.5, forward=True)
+fig.set_size_inches(15, 15, forward=True)
 
 x_min = -3
 x_max = 3
@@ -16,8 +16,8 @@ y_min = -3
 y_max = 3
 
 # The transformation matrix
-A = np.array([  [2, 0.5],
-                [0.2, 1]])
+A = 2*np.array([  [np.sqrt(3)/2, 0.5],
+                [-0.5, np.sqrt(3)/2]])
 
 # The SVD of the transformation matrix
 U, S, V_T = lin.svd(A)
@@ -27,10 +27,17 @@ print(U)
 print(S)
 print(V_T)
 
+# X = np.array([  [0, 0],
+#                 [np.sqrt(2)/2, np.sqrt(2)/2],
+#                 [np.sqrt(2)/2, -np.sqrt(2)/2],
+#                 [0, 0]]).T
+
 X = np.array([  [0, 0],
-                [np.sqrt(2)/2, np.sqrt(2)/2],
-                [np.sqrt(2)/2, -np.sqrt(2)/2],
+                [1, 0],
+                [1, 1],
+                [0, 1],
                 [0, 0]]).T
+
 X_orginal = np.eye(2).dot(X)
 
 ax[0, 0].plot(X[0, :], X[1, :], color=sns.cubehelix_palette(8)[2])
@@ -54,7 +61,7 @@ ax[1, 0].set_ylim([y_min, y_max])
 ax[1, 0].set_title("Original $S V^T x$", size=20)
 
 X = U.dot(X)
-# X = A.dot(X_orginal)
+X = A.dot(X_orginal)
 ax[1, 1].plot(X[0, :], X[1, :], color=sns.cubehelix_palette(8)[2])
 ax[1, 1].fill(X[0, :], X[1, :], color=sns.cubehelix_palette(8)[2])
 ax[1, 1].set_xlim([x_min, x_max])
